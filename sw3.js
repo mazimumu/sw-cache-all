c2e674b87f3d47c99a5ae882ee315e13005935d7
Danni Le <danni@Dannis-MacBook-Air.local>
2016-03-29 00:32:39 +0200

    modified sw3

--- sw3.js
+++ sw3.js
@@ -1,6 +1,7 @@
+var CACHE_NAME = 'offline';
 this.addEventListener('install', function(event) {
 	event.waitUntil(
-		caches.open('offline').then(function(cache) {
+		caches.open(CACHE_NAME).then(function(cache) {
 			return cache.addAll([
 				'/',
 				'/index.htm',
@@ -13,14 +14,41 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('offline').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }));
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have 2 stream.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
